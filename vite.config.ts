418c4d13fa7219697a60a2ef781efa0eaf65941c
GuillemetSteven <berryfart2@gmail.com>
2025-12-22 17:20:24 +0100

    GitHub pages changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Pour GitHub Pages - chemins relatifs
+  base: '/Digital_Economy_Karmine_Web_App/', // Pour GitHub Pages - chemins relatifs
 })
